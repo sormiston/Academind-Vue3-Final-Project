48f3624fe3d84be73eea00c6936cd1c2b3fc2912
SOrmiston <sean.david.ormiston@gmail.com>
2021-01-16 16:26:21 -0500

    TODO Caching HTTP response data

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import { createApp } from 'vue';
-import router from './router.js'
-import store from './vuex-store/index.js'
-import App from './App.vue'
+import router from './router.js';
+import store from './vuex-store/index.js';
+import App from './App.vue';
 import BaseBadge from './components/baseUI/BaseBadge.vue';
 import BaseButton from './components/baseUI/BaseButton.vue';
-import BaseCard from './components/baseUI/BaseCard.vue'
-const app = createApp(App)
+import BaseCard from './components/baseUI/BaseCard.vue';
+import BaseSpinner from './components/baseUI/BaseSpinner.vue'
+import BaseDialog from './components/baseUI/BaseDialog.vue'
 
-app.use(router)
-app.use(store)
-
-app.component('base-badge', BaseBadge)
-app.component('base-button', BaseButton);
-app.component('base-card', BaseCard)
-
-app.mount('#app');
+createApp(App)
+  .use(router)
+  .use(store)
+  .component('base-badge', BaseBadge)
+  .component('base-button', BaseButton)
+  .component('base-card', BaseCard)
+  .component('base-spinner', BaseSpinner)
+  .component('base-dialog', BaseDialog)
+  .mount('#app');
